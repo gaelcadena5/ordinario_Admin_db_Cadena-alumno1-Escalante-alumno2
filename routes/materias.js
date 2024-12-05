1d8e05e3dfa939075a8f7d3b53f10a07d8c291b4
Tartamannnn <popolol5342@gmail.com>
2024-12-05 00:59:32 -0600

    Materia endpoint post get

--- routes/materias.js
+++ routes/materias.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const databaseHandler = require('../sql/databaseHandler');
 const router = express.Router();
 
-let materias = [];
+const handler = new databaseHandler()
 
-router.get('/', (req, res) => {
-    res.status(200).json(materias);
+router.get('/', async (req, res) => {
+    try {
+        const response = await handler.getMaterias();
+        return res.status(200).json(response);
+    } catch (error) {
+        console.error('Error al obtener materias:', error);
+        return res.status(500).json({ error: 'Error al obtener las materias' });
+    }
 });
 
-router.post('/', (req, res) => {
-    const { nombre, profesor_id } = req.body;
-    const nuevaMateria = { id: materias.length + 1, nombre, profesor_id };
-    materias.push(nuevaMateria);
-    res.status(201).json({ mensaje: 'Materia creada', materia: nuevaMateria });
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const response = await handler.getMateriaById(id);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.error('Error al obtener materias:', error);
+        return res.status(500).json({ error: 'Error al obtener las materias' });
+    }
+});
+router.post('/addMateria', (req, res) => {
+    const nuevaMateria = req.body;
+    try {
+        const response = handler.insertMateria(nuevaMateria);
+        return res.status(201).json({ message: 'Materia insertada correctamente', materia: nuevaMateria, response: response });
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al insertar la materia' });
+    }
 });
 
 module.exports = router;
