663dac478b5fb898c8ff48f0ae983894f6907497
Tartamannnn <popolol5342@gmail.com>
2024-12-04 21:43:04 -0600

    Installed dependencies

--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const databaseHandler = require('../sql/databaseHandler');
 const router = express.Router();
 
 let estudiantes = [];
 
 router.get('/', (req, res) => {
+    const estudiantes = databaseHandler.getEstudiantes();
     res.status(200).json(estudiantes);
 });
 
-router.post('/', (req, res) => {
-    const { nombre, apellidos, email, matricula, edad, semestre } = req.body;
-    const nuevoEstudiante = { id: estudiantes.length + 1, nombre, apellidos, email, matricula, edad, semestre };
-    estudiantes.push(nuevoEstudiante);
+router.post('/addEstudiante', (req, res) => {
+    const nuevoEstudiante = req.body;
+    databaseHandler.insertEstudiante(nuevoEstudiante);
     res.status(201).json({ mensaje: 'Estudiante creado', estudiante: nuevoEstudiante });
 });
 
