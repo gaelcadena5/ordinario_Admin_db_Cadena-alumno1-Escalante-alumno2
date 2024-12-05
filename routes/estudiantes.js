d2c533fd30223542200a7c9e193a566e09d30634
Tartamannnn <popolol5342@gmail.com>
2024-12-05 00:09:36 -0600

    Added a get individual estudiante

--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -14,7 +14,17 @@ router.get('/',  async (req, res) => {
         return res.status(500).json({ error: 'Error al obtener los estudiantes'});
     }
 });
-
+router.get('/:id',  async (req, res) => {
+    const id = req.params.id;
+    try {
+        const response = await handler.getEstudianteById(id);
+        return res.status(200).json(response);
+    }
+    catch (error) {
+        console.error('Error al obtener estudiantes:', error);
+        return res.status(500).json({ error: 'Error al obtener los estudiantes'});
+    }
+});
 router.post('/addEstudiante', (req, res) => {
     const nuevoEstudiante = req.body;
     try {
